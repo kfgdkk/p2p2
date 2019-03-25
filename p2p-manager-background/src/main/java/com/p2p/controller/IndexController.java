d65eae6a96a119e2eaf18e5d3b5ea653c4233189
kfgdkk <kfgdkk@163.com>
2019-03-25 20:23:28 +0800

    修改了注释

--- p2p-manager-background/src/main/java/com/p2p/controller/IndexController.java
+++ p2p-manager-background/src/main/java/com/p2p/controller/IndexController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
     @RequestMapping("/")
     public String index(){
+        //bug修复
         return "success";
     }
 }
